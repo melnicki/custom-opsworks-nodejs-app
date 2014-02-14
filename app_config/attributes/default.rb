bb42cf289f917230af0625c6b9155f40ea6d049a
Mike Melnicki <melnicki@gmail.com>
2014-02-14 13:14:51 -0800

    Fixed the setup to install a custom NodeJS version

--- app_config/attributes/default.rb
+++ app_config/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:version] = '0.10.25'
+default[:nodejs_custom][:version] = '0.10.25'
 default[:phantomjs][:version] = '1.9.7'
 default[:phantomjs][:src_url] = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{node[:phantomjs][:version]}-linux-x86_64.tar.bz2"
