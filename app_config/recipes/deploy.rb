b9e425c5b67d26bd1784cb1b7b95a14d05e7d530
Mike Melnicki <melnicki@gmail.com>
2014-02-14 18:17:05 -0800

    Try restarting the app

--- app_config/recipes/deploy.rb
+++ app_config/recipes/deploy.rb
@@ -33,6 +33,10 @@ link "/usr/local/bin/npm" do
   end
 end
 
+service 'monit' do
+  action :nothing
+end
+
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'nodejs'
     Chef::Log.debug("Skipping deploy::nodejs application #{application} as it is not a node.js app")
@@ -55,6 +59,16 @@ node[:deploy].each do |application, deploy|
 
   link "#{deploy[:deploy_to]}/current/config/config.json" do
     to "#{deploy[:deploy_to]}/shared/config/config.json"
+  end
+
+  directory "#{deploy[:deploy_to]}/current/node_modules" do
+    action :delete
+  end
+
+  execute "npm install" do
+    cwd "#{deploy[:deploy_to]}/current"
+    command "/usr/local/bin/npm install --production"
+    notifies :restart, "service[monit]", :immediately
   end
 
 end
