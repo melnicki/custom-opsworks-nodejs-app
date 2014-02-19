2b5e6ca5c093043589de6e064ca7a5013c6f6551
Mike Melnicki <melnicki@gmail.com>
2014-02-19 12:09:08 -0800

    Override the symlinks hash

--- app_config/attributes/default.rb
+++ app_config/attributes/default.rb
@@ -1,2 +1,9 @@
 default[:phantomjs][:version] = '1.9.7'
 default[:phantomjs][:src_url] = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{node[:phantomjs][:version]}-linux-x86_64.tar.bz2"
+
+node[:deploy].each do |application, deploy|
+  override[:deploy][application][:symlink_before_migrate] = {
+    "config/config.json" => "config/config.json"
+  }
+end
+
