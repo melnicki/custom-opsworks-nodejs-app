2ce8484bd90887528ce63fe47247e1800754f9d1
Mike Melnicki <melnicki@gmail.com>
2014-02-19 10:36:43 -0800

    Removing unnecessary override

--- app_config/attributes/default.rb
+++ app_config/attributes/default.rb
@@ -1,3 +1,2 @@
-override[:opsworks_nodejs][:version] = '0.10.25'
 default[:phantomjs][:version] = '1.9.7'
 default[:phantomjs][:src_url] = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{node[:phantomjs][:version]}-linux-x86_64.tar.bz2"
